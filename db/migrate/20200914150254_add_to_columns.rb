cf5a4018d811799249d665fb7b63e218ff3a28e3
Din Abarbanel <din14000@gmail.com>
2020-09-22 14:46:27 +0300

    Changes to be committed: 	deleted:    db/migrate/20200914142447_create_orders_table.rb 	deleted:    db/migrate/20200914142643_create_activities_table.rb 	deleted:    db/migrate/20200914143204_add_devise_to_users.rb 	deleted:    db/migrate/20200914144443_create_active_storage_tables.active_storage.rb 	deleted:    db/migrate/20200914144444_create_action_text_tables.action_text.rb 	deleted:    db/migrate/20200914150254_add_to_columns.rb 	deleted:    db/migrate/20200922113026_create_users.rb 	new file:   db/migrate/20200922114214_create_database_structure.rb

--- db/migrate/20200914150254_add_to_columns.rb
+++
@@ -1,6 +0,0 @@
-class AddToColumns < ActiveRecord::Migration[6.0]
-  def change
-    add_timestamps(:orders)
-    add_timestamps(:activities)
-  end
-end
---
+++ db/migrate/20200922114214_create_database_structure.rb
@@ -0,0 +1,73 @@
+class CreateDatabaseStructure < ActiveRecord::Migration[6.0]
+  def change
+    create_table "action_text_rich_texts", force: :cascade do |t|
+      t.string "name", null: false
+      t.text "body"
+      t.string "record_type", null: false
+      t.integer "record_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.index ["record_type", "record_id", "name"], name: "index_action_text_rich_texts_uniqueness", unique: true
+    end
+  
+    create_table "active_storage_attachments", force: :cascade do |t|
+      t.string "name", null: false
+      t.string "record_type", null: false
+      t.integer "record_id", null: false
+      t.integer "blob_id", null: false
+      t.datetime "created_at", null: false
+      t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+      t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+    end
+  
+    create_table "active_storage_blobs", force: :cascade do |t|
+      t.string "key", null: false
+      t.string "filename", null: false
+      t.string "content_type"
+      t.text "metadata"
+      t.bigint "byte_size", null: false
+      t.string "checksum", null: false
+      t.datetime "created_at", null: false
+      t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+    end
+  
+    create_table "activities", force: :cascade do |t|
+      t.string "title"
+      t.integer "max_attendees"
+      t.integer "ticket_price"
+      t.integer "tickets_left"
+      t.integer "orders_id"
+      t.date "date"
+      t.string "owner"
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.index ["orders_id"], name: "index_activities_on_orders_id"
+    end
+  
+    create_table "orders", force: :cascade do |t|
+      t.integer "number_of_tickets"
+      t.integer "total_order_price"
+      t.integer "user_id", null: false
+      t.integer "activity_id", null: false
+      t.datetime "created_at", precision: 6, null: false
+      t.datetime "updated_at", precision: 6, null: false
+      t.index ["activity_id"], name: "index_orders_on_activity_id"
+      t.index ["user_id"], name: "index_orders_on_user_id"
+    end
+  
+    create_table "users", force: :cascade do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
+  
+    add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+    add_foreign_key "orders", "activities"
+    add_foreign_key "orders", "users"
+    end
+  end
+end
