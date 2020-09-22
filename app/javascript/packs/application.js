819796ee06690f0d738fd6fd2e070eb3658adf71
Din Abarbanel <din14000@gmail.com>
2020-09-22 18:32:41 +0300

    Changes to be committed: 	renamed:    app/assets/stylesheets/style.scss -> app/assets/stylesheets/application.scss 	modified:   app/javascript/packs/application.js 	modified:   config/webpack/environment.js 	modified:   config/webpacker.yml 	modified:   db/schema.rb     modified:   package.json

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+var jQuery = require("jquery");
+
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
@@ -17,8 +19,8 @@ require("channels")
 require("trix")
 require("@rails/actiontext")
 
-//= require jquery3
-//= require popper
-//= require bootstrap
-import 'bootstrap'
-import '../stylesheets/style'
+// import jQuery from "jquery";
+global.$ = global.jQuery = jQuery;
+window.$ = window.jQuery = jQuery;
+
+require("bootstrap");
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_22_112742) do
+ActiveRecord::Schema.define(version: 2020_09_22_114214) do
 
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
