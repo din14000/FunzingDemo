1f070776f698b8e3b8ad15dd2d3913180d4c49e6
Din Abarbanel <din14000@gmail.com>
2020-10-21 18:25:31 +0300

    new file:   app/views/pages/server.js 	modified:   package.json 	modified:   webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,77 +1,39 @@
-const path = require('path');
-const webpack = require('webpack');
-
-
-
-
-/*
- * SplitChunksPlugin is enabled by default and replaced
- * deprecated CommonsChunkPlugin. It automatically identifies modules which
- * should be splitted of chunk by heuristics using module duplication count and
- * module category (i. e. node_modules). And splits the chunks…
- *
- * It is safe to remove "splitChunks" from the generated configuration
- * and was added as an educational example.
- *
- * https://webpack.js.org/plugins/split-chunks-plugin/
- *
- */
-
-/*
- * We've enabled TerserPlugin for you! This minifies your app
- * in order to load faster and run less javascript.
- *
- * https://github.com/webpack-contrib/terser-webpack-plugin
- *
- */
-
-const TerserPlugin = require('terser-webpack-plugin');
-
-
-
-
 module.exports = {
-  mode: 'development',
-  entry: './sec.js',
-  plugins: [new webpack.ProgressPlugin()],
-
-  module: {
-    rules: [{
-      test: /.(scss|css)$/,
-
-      use: [{
-        loader: "style-loader"
-      }, {
-        loader: "css-loader",
-
-        options: {
-          sourceMap: true
-        }
-      }, {
-        loader: "sass-loader",
-
-        options: {
-          sourceMap: true
-        }
-      }]
-    }]
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
   },
-
-  optimization: {
-    minimizer: [new TerserPlugin()],
-
-    splitChunks: {
-      cacheGroups: {
-        vendors: {
-          priority: -10,
-          test: /[\\/]node_modules[\\/]/
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
         }
       },
-
-      chunks: 'async',
-      minChunks: 1,
-      minSize: 30000,
-      name: true
-    }
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "style-loader"
+          },
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+              importLoaders: 1,
+              localIdentName: "[name]_[local]_[hash:base64]",
+              sourceMap: true,
+              minimize: true
+            }
+          }
+        ]
+      }
+    ]
   }
-}
+};
